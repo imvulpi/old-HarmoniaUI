011308a9a1739c69d00b05f0d0eddcf2841a8a28
Vulpi <78976654+imvulpi@users.noreply.github.com>
2024-12-04 21:57:48 +0100

    Margins, backgrounds, split, and bugfixes (#4)
    * Margins, backgrounds, split, and bugfixes
    
    Adds margins up right down left.
     - Note: for this specific container the right margin is not used, this is because the base container flow makes it unnecessary, however the right margin will be used in different container types.
    
    Added LayoutChanged values into enum for Margin and padding (will be added later).
    
    Adds background color possibility using draw notifications and canvas item draw functions.
    Adds split function into string helper file.
    
    Fixes replace function in string helper file, where previously the function would run in a loop to decrease the index, now the index value is simply added into iterator
    
    Fixes length pair and other variables without default value, now replaced with default initialization value of 0. Before fix this caused the unset values like positions/sizes and other to have unwanted garbage values where really 0 was wanted.
    
    * Documentations and minor fixes.
    
    Added documentation and small features or fixes.
    
    * Additional functions open to API
    
    Added setters and getters function to the script API.
    Fixed problem with AlertLayoutChange being not registered

--- src/core/systems/alert/layout/alert_layout_change.cpp
+++ src/core/systems/alert/layout/alert_layout_change.cpp
@@ -5,3 +5,12 @@ AlertLayoutChange::AlertLayoutChange(String name, LayoutChanged changed)
     alert_name = name;
     layout_changed = changed;
 }
+
+void AlertLayoutChange::_bind_methods()
+{
+    BIND_ENUM_CONSTANT(AlertLayoutChange::WIDTH);
+    BIND_ENUM_CONSTANT(AlertLayoutChange::HEIGHT);
+    BIND_ENUM_CONSTANT(AlertLayoutChange::POSITION);
+    BIND_ENUM_CONSTANT(AlertLayoutChange::MARGIN);
+    BIND_ENUM_CONSTANT(AlertLayoutChange::PADDING);
+}
