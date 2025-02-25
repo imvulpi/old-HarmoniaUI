f4deadc14a4c9f1a263091af9543249a9a64cf40
imvulpi <78976654+imvulpi@users.noreply.github.com>
2025-02-25 01:12:55 +0100

    Uploaded CSharp Bindings
    C# Bindings for HarmoniaUI version 2.0.0 Alpha,
    
    if you use .NET/C# version of Godot then upload the csharp directory anywhere in your project.
    (preferably harmoniaui directory to keep it organized)
    
    Enjoy!

---
+++ bindings/csharp/core/systems/alert/AlertCallable.cs
@@ -0,0 +1,25 @@
+﻿using Godot;
+using System;
+
+namespace HarmoniaUI.core.systems.alert
+{
+    public partial class AlertCallable : GodotObject
+    {
+        public AlertCallable()
+        {
+
+        }
+        public event EventHandler<AlertWrapper> AlertCalled;
+        public Callable GetCallable()
+        {
+            Callable callable = new(this, nameof(Callback));
+            return callable;
+        }
+
+        private void Callback(GodotObject godotObject)
+        {
+            AlertWrapper alertWrapper = new(godotObject);
+            AlertCalled.Invoke(this, alertWrapper);
+        }
+    }
+}
---
+++ bindings/csharp/core/systems/alert/AlertManager.cs
@@ -0,0 +1,31 @@
+﻿using Godot;
+
+namespace HarmoniaUI.core.systems.alert
+{
+    public partial class AlertManager : GodotObject
+    {
+        public AlertManager(GodotObject instance)
+        {
+            _instance = instance;
+        }
+        public AlertManager()
+        {
+            _instance = ClassDB.Instantiate("AlertManager").AsGodotObject();
+        }
+        private GodotObject _instance;
+        public void DispatchAlert(AlertWrapper alert)
+        {
+            _instance.Call("dispatch_alert", alert._instance);
+        }
+
+        public void AddBind(string alertType, Callable callable)
+        {
+            _instance.Call("add_bind", alertType, callable);
+        }
+
+        public void RemoveBind(string alertType, Callable callable)
+        {
+            _instance.Call("remove_bind", alertType, callable);
+        }
+    }
+}
