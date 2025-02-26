3ac029a71a17732067425b65df74bdad18dab4be
imvulpi <78976654+imvulpi@users.noreply.github.com>
2025-02-27 00:05:44 +0100

    C# Bindings documentation and location change
    Added documentation to the C# Bindings for 2.0.0 Alpha.
    AlertLayoutChangeWrapper is undocumented, there are not enough public api yet to differentiate it from normal Alert.
    
    Location change:
    Moved from bindings/... to extensions/...
    In this case extensions/csharp
    
    Include csharp directory in harmoniaui directory or next to .gdextension file
    
    For ease of development in C# I'm developing harmoniaui-editor
    
    Take care!
    2.0.0 Alpha C# bindings.

---
+++ extensions/csharp/core/systems/alert/AlertWrapper.cs
@@ -0,0 +1,45 @@
+﻿using Godot;
+
+namespace HarmoniaUI.core.systems.alert
+{
+    /// <summary>
+    /// Represents a wrapper for an internal alert instance, providing functionality to manage alert.
+    /// Automatically creates an internal alert instance upon instantiation.
+    /// </summary>
+    public class AlertWrapper
+    {
+        public AlertWrapper(GodotObject godotObject)
+        {
+            _instance = godotObject;
+        }
+
+        public AlertWrapper()
+        {
+            _instance = ClassDB.Instantiate("Alert").AsGodotObject();
+        }
+
+        /// <summary>
+        /// Internal instance of the alert.  
+        /// This instance is created when the wrapper is instantiated.
+        /// </summary>
+        public GodotObject _instance;
+
+        /// <summary>
+        /// Sets the name of the alert.
+        /// </summary>
+        /// <param name="alertName">The name to assign to the alert.</param>
+        public void SetAlertName(string alertName)
+        {
+            _instance.Call("set_alert_name", alertName);
+        }
+
+        /// <summary>
+        /// Gets the name of the alert.
+        /// </summary>
+        /// <returns>The current alert name.</returns>
+        public string GetAlertName()
+        {
+            return _instance.Call("get_alert_name").AsString();
+        }
+    }
+}
