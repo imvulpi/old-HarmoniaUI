f174a9c3272f98683d0bf8ded9b3c559af859f7b
imvulpi <78976654+imvulpi@users.noreply.github.com>
2025-02-27 00:19:33 +0100

    HarmoniaUI Editor Plugin
    Added developed HarmoniaUI Editor plugin
    Recommended for C# Development for it's auto create functionality.
    
    Explanation:
    With Godot 4.3, making GDExtension classes accessible in C# is a hard task. This is why HarmoniaUI has C# bindings.
    
    Unfortunately Godot can't convert a it's C++ native class to our binding, for Harmonia Nodes to be accessible in Godot API they have to have a script which in implements the binding, it can be your own custom class or just an empty class but it has to be there.
    
    This is where the plugin comes in, it automatically attaches empty classes in newly created or loaded by visiting a scene nodes.
    
    - Does this limit us at all?
    No, you are still able to create custom scripts, the plugin only attaches empty scripts to nodes without any scripts. If you want to develop a custom script in a Node which has the empty script you can just remove the empty script and create your own custom one.
    Empty script have a purple block icon, depending on chosen harmonia location you might need to adjust the path.
    Custom scripts (created by you) wont get removed by plugin.
    
    Enjoy!

---
+++ bindings/harmoniaui-editor/HarmoniaEditorConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harmoniaui_editor
+{
+    public class HarmoniaEditorConstants
+    {
+        public const string AUTO_CREATION_SETTING = "harmoniaui/auto_creation_enabled";
+        public const string CSHARP_BINDINGS_PATH_SETTING = "harmoniaui/csharp_bindings_path";
+        public const string CONTAINERS_DIR = "containers";
+        public const string CONTAINER_BOX_SCRIPT_NAME = "EmptyContainerBox.cs";
+        public const string CONTAINER_INTERNAL_CLASS_NAME = "ContainerBox";
+    }
+}
